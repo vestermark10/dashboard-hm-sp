cacc49b0a1ea6d943b1873e9c52f5e0337b4d027
JG-Ehmsen <jeppeehmsen@gmail.com>
2026-01-27 16:19:06 +0100

    Update oneConnectScraper.mjs

--- services/oneConnectScraper.mjs
+++ services/oneConnectScraper.mjs
@@ -32,6 +32,7 @@ class OneConnectScraper {
     console.log('OneConnect: Starter browser...');
 
     this.browser = await puppeteer.launch({
+      executablePath: '/usr/bin/chromium',
       headless: 'new',
       args: [
         '--no-sandbox',
